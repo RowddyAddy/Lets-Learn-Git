5a537653c920e21950dbb08c9416dad6ec3180e2
Aditya Rahangdale <adityarahangdale1999@gmail.com>
2023-05-01 16:19:06 +0530

    added mutifly.java

---
+++ mutifly.java
@@ -0,0 +1,9 @@
+public class mutifly {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        int mul;
+        mul = a * b;
+        System.out.println(+mul);
+    }
+}
